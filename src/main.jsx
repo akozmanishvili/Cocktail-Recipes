68e5c5fbc49930e8b2af15c26747f5f8368b6f97
AMIKO <akozmanishvili@gmail.com>
2026-06-26 18:53:30 +0400

    Commit: added router in main.jsx, created 4 page files in pages folder (Cocktails.jsx, SingleCocktail.jsx, Home.jsx, Error.jsx)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Error from "./pages/Error.jsx";
+import Home from "./pages/Home.jsx";
+import Cocktails from "./pages/Cocktails.jsx";
+import SingleCocktail from "./pages/SingleCocktail.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    errorElement: <Error></Error>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>,
+        path: "cocktails",
+        element: <Cocktails></Cocktails>,
+        path: "cocktails/:id",
+        element: <SingleCocktail></SingleCocktail>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProviderouter router={router} />
   </StrictMode>,
-)
+);
