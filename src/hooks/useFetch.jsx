df6b19b9a9680dc6da1a7fa99f8536cfaf230b23
AMIKO <akozmanishvili@gmail.com>
2026-06-26 20:59:42 +0400

    commit: create useFetch in hook folder. It fetches url for API

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, SetIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  const controller = new AbortController();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(url, controller.signal);
+        const json = await response.json();
+        setData(json);
+      } catch (err) {
+        console.error(`error` + err.message);
+        setError(err.message);
+      } finally {
+        SetIsLoading(false);
+      }
+    }
+
+    fetchData();
+
+    return controller.abort;
+  }, [url]);
+  return { data, error, isLoading };
+};
+
+export default useFetch;
