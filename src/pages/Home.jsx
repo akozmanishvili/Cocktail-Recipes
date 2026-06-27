de0acdf4eb44ed3ecdaddaff9c2062b1fe314b93
AMIKO <akozmanishvili@gmail.com>
2026-06-27 22:51:13 +0400

    commit: modified home.jsx page. Now it doesnt look bland and sbows info of random cocktail

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,47 @@
+import useFetch from "../hooks/useFetch";
+import CleanDrinkData from "../utils/CleanDrinkData";
+import { Link } from "react-router-dom";
 const Home = () => {
-  return <h2 style={{ textAlign: "center" }}>Home Page</h2>;
+  const { data, error, isLoading } = useFetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/random.php`,
+  );
+
+  if (isLoading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error 404 - Page not Found</h2>;
+
+  const drink = data?.drinks ? CleanDrinkData(data.drinks[0]) : null;
+
+  if (!drink) {
+    return <h2>Something went wrong...</h2>;
+  }
+
+  const { name, image, category, glass, alcoholic, instructions, ingredients } =
+    drink;
+
+  return (
+    <div className="home">
+      <div className="home-hero">
+        <h1>Discover your Favorite Next COCKTAIL</h1>
+        <p>Search hundreds of cocktail recipes from around the world.</p>
+      </div>
+      <div className="featured">
+        <h2>Featured Cocktail</h2>
+        <div className="single-cocktail-content">
+          <img src={image} alt={name}></img>
+          <div className="single-cocktail-info">
+            <h3>Name: {name}</h3>
+            <p>Category: {category}</p>
+            <p>Alcoholic type: {alcoholic}</p>
+            <h3>Ingredients</h3>
+            <Link to={"/cocktails/${id}"} className="hero-btn">
+              {" "}
+              See Recipe{" "}
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
